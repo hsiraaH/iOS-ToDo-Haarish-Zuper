36bac35881147b4b13474668dba73d5f678ab5ef
KG Haarish Kannan <haarishkannanii@gmail.com>
2022-02-24 11:44:24 +0530

    added compositional layout to todo screen

--- Haarish ToDo Zuper/Haarish ToDo Zuper/Controllers/TodoViewController.swift
+++ Haarish ToDo Zuper/Haarish ToDo Zuper/Controllers/TodoViewController.swift
@@ -7,8 +7,37 @@
 
 import UIKit
 
+class TodoCell: UICollectionViewCell {
+    @IBOutlet weak var borderView: UIView! {
+        didSet {
+            borderView.layer.cornerRadius = 10
+        }
+    }
+    @IBOutlet weak var tickImage: UIImageView!
+    @IBOutlet weak var tickCircle: UIView! {
+        didSet {
+            tickCircle.layer.cornerRadius = tickCircle.frame.height / 2
+            tickCircle.layer.shouldRasterize = true
+        }
+    }
+    @IBOutlet weak var tagLabel: UILabel! {
+        didSet {
+            tagLabel.layer.cornerRadius = 6
+            tagLabel.layer.masksToBounds = true
+        }
+    }
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var priorityCircle: UIView!
+    let identifier = "TodoCell"
+}
+
 class TodoViewController: UIViewController {
-    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchBar: UISearchBar! {
+        didSet {
+            searchBar.backgroundImage = UIImage()
+        }
+    }
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,9 +51,41 @@ class TodoViewController: UIViewController {
             self.navigationController?.navigationBar.standardAppearance = navBarAppearance
             self.navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
         }
-        searchBar.backgroundImage = UIImage()
+        
+        if #available(iOS 13.0, *){
+            collectionView.collectionViewLayout = createCompositionalLayout()
+        }
+        else {
+            
+        }
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-
+    
+    @available(iOS 13.0, *)
+    func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(120))
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 1)
+                
+                let section = NSCollectionLayoutSection(group: group)
+                return section
+        }
+        return layout
+    }
 
 }
 
+extension TodoViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
